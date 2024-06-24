e705fa3f23a66ef7ce1ccf5431b8f69357128e3a
Zelda170603 <antonio.170603@gmail.com>
2024-06-24 15:02:02 -0600

    message module actualized

---
+++ resources/views/Productos/prueba.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+
+    <section class="bg-gray-50 py-8 antialiased dark:bg-gray-900 md:py-12">
+        <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
+            <!-- Heading & Filters -->
+            <div class="mb-4 grid gap-4 sm:mb-8 grid-cols-4">
+
+
+                
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
